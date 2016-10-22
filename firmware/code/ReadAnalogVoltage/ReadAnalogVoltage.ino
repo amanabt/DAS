7505890eddf0a1e29501b4b3952c812d2c47d1c1
amanabt <aman2official@gmail.com>
2016-10-22 11:14:59 +0530

    Analog Read renamed

--- firmware/code/ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ firmware/code/ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -19,7 +19,7 @@ void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+  float voltage = sensorValue * (1.1 / 1023.0);
   // print out the value you read:
   Serial.println(voltage);
 }
